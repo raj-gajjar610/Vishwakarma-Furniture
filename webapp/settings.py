0489e176e5bc8aa5f466cdb52719bf97d65062a6
raj-gajjar610 <rajgajjjar7@gmail.com>
2026-07-02 16:02:14 +0530

    Fix static files configuration

--- webapp/settings.py
+++ webapp/settings.py
@@ -126,9 +126,6 @@ USE_TZ = True
 
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = 'media/'
